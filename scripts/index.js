375e46dc05b8ad3a1d9bfc577f6126f203ed0080
Ivekou Brahim vall <ivekoudah@gmail.com>
2026-01-22 09:59:25 -0500

    spot8

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 const initialCards = [
   {
+    name: "Golden Gate Bridge",
+    link: " https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
@@ -31,17 +36,17 @@ const newPostProfileModal = document.querySelector("#new-post-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const editProfileForm = editProfileModal.querySelector(".modal__form");
 const editProfileNameInput = editProfileModal.querySelector(
-  "#profile-name-input"
+  "#profile-name-input",
 );
 const editDescriptionInput = editProfileModal.querySelector(
-  "#profile-description-input"
+  "#profile-description-input",
 );
 
 const newPostProfileForm = newPostProfileModal.querySelector(".modal__form");
 const newPostProfileImageLink =
   newPostProfileModal.querySelector("#card-image-input");
 const newPostProfileCaption = newPostProfileModal.querySelector(
-  "#profile-caption-input"
+  "#profile-caption-input",
 );
 
 const newPostBtn = document.querySelector(".profile__add-btn");
@@ -51,12 +56,57 @@ const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+const previewModal = document.querySelector("#preview__type_modal");
+const previewImageEl = previewModal.querySelector(".modal__image");
+const previewCaption = previewModal.querySelector(".modal__caption");
+
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  cardTitleEl.textContent = data.name;
+
+  const cardLikeBtnEl = cardElement.querySelector(".card__like-btn");
+  cardLikeBtnEl.addEventListener("click", () => {
+    cardLikeBtnEl.classList.toggle("card__like-btn_active");
+  });
+
+  const cardDeleteBtnEl = cardElement.querySelector(".card__delete-btn");
+  cardDeleteBtnEl.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImageEl.addEventListener("click", () => {
+    previewImageEl.src = data.link;
+    previewModal.alt = data.name;
+    previewCaption.textContent = data.name;
+    openModal(previewModal);
+  });
+
+  return cardElement;
+}
+
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
+  modal
+    .querySelector(".modal__close-btn")
+    .addEventListener("click", () => closeModal(modal));
 }
 
 function closeModal(modal) {
   modal.classList.remove("modal_is-opened");
+  modal
+    .querySelector(".modal__close-btn")
+    .removeEventListener("click", () => closeModal(modal));
 }
 
 editProfileBtn.addEventListener("click", function () {
@@ -91,16 +141,20 @@ function handleNewPostSubmit(evt) {
 
   //const newPostImageLink = newPostProfileImageLink.value;
   //const newPostCaption = newPostProfileCaption.value;
+  const inputValue = {
+    link: newPostProfileImageLink.value,
+    name: newPostProfileCaption.value,
+  };
 
-  console.log(newPostProfileImageLink.value);
-  console.log(newPostProfileCaption.value);
-
+  const cardElement = getCardElement(inputValue);
+  cardsList.prepend(cardElement);
+  evt.target.reset();
   closeModal(newPostModal);
 }
 
 newPostProfileForm.addEventListener("submit", handleNewPostSubmit);
 
 initialCards.forEach(function (item) {
-  console.log(item.name);
-  console.log(item.link);
+  const cardElement = getCardElement(item);
+  cardsList.append(cardElement);
 });
